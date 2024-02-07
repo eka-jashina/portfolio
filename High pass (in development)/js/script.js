37a85880cdf8e92fde7e9252e9b116dfced7cf7a
miokka <89094441+miokka@users.noreply.github.com>
2024-02-07 21:57:14 +0300

    add changes

--- High pass (in development)/js/script.js
+++ High pass (in development)/js/script.js
@@ -1,10 +1,31 @@
-var block = document.querySelector('.map__wrapper');
-var button= document.querySelector('.address__button');
+let burger = document.querySelector('.burger');
+let navList = document.querySelector('.nav__list');
 
-button.onclick = function() {
+burger.addEventListener('click', function() {
+  burger.classList.toggle('burger_active');
+  navList.classList.toggle('nav__list_active');
+});
+
+let block = document.querySelector('.map__wrapper');
+let button = document.querySelector('.address__button');
+let map = document.querySelector('.map');
+
+// При клике на карту, блок будет отображаться
+map.addEventListener('click', function() {
   if (!block) {
     return;
   }
 
-  block.hidden= !block.hidden;
-};
+  block.hidden = false;
+});
+
+button.addEventListener('click', function(event) {
+  event.stopPropagation(); // Остановка всплытия события клика на кнопке, чтобы не привести к отображению блока адреса
+
+  if (!block) {
+    return;
+  }
+
+  // При нажатии на кнопку, блок будет скрываться
+  block.hidden = true;
+});
